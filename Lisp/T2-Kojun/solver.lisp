72a90da20142ad717c2f465bfef9bec4e86a2c21
Gabriel Vargas <gabriel.vargas.coelho@gmail.com>
2022-11-03 11:04:13 -0300

    Add funcoes iniciais de validacao

--- Lisp/T2-Kojun/solver.lisp
+++ Lisp/T2-Kojun/solver.lisp
@@ -70,6 +70,10 @@
   (nth 2 elem)
 )
 
+(defun getBoardValues (board)
+  (matrixMap (lambda (elem all) (getElemValues elem)) board)
+)
+
 (defun filterById (id list)
   (if (null list)
     ()
@@ -121,12 +125,19 @@
       )
       (list (difference initialList existingValues) (getElemId elem) (getElemIdx elem))
     )
-    elem
+    (list 
+      (difference (getElemValues elem) (valuesInGroup board (getElemId elem)))
+      (getElemId elem)
+      (getElemIdx elem)
+    )
   )
 )
 
 (defun difference (list1 list2)
-  (filter (lambda (elem) (not (member elem list2))) list1)
+  (if (= (len list1) 1)
+    list1
+    (filter (lambda (elem) (not (member elem list2))) list1)
+  )
 )
 
 (defun possibleChoices (board)
@@ -151,23 +162,96 @@
   (= (len (difference arr1 arr2)) 0)
 )
 
+(defun validNeighbours (list)
+  (if (single list)
+    T
+    (let (
+      (a (first list))
+      (b (first (rest list)))
+      (c (rest (rest list)))
+      )
+      (if (and (single (list a)) (single (list b)))
+        (if (= a b)
+          NIL
+          (validNeighbours (cons b c))
+        )
+        (validNeighbours (cons b c))
+      )
+    )
+  )
+)
+
+(defun decrease (list)
+  (if (single list)
+    T
+    (let (
+      (a (first list))
+      (b (first (rest list)))
+      (c (rest (rest list)))
+      )
+      (if (and (single (list a)) (single (list b)))
+        (if (< a b)
+          NIL
+          (decrease (cons b c))
+        )
+        (decrease (cons b c))
+      )
+    )
+  )
+)
+
+(defun valid (board)
+  (and
+    (every (function validNeighbours) (getBoardValues board))
+    (every (function validNeighbours) (transpose (getBoardValues board)))
+  )
+)
+
+(defun cars (matrix)
+  "Return a list with all the cars of the lists in matrix"
+  (if (null matrix)
+    NIL
+    (cons (car (car matrix)) (cars (cdr matrix)))
+  )
+)
+
+(defun cdrs (matrix)
+  "Return a list with all the cdrs of the lists in matrix"
+  (if (null matrix)
+    NIL
+    (cons (cdr (car matrix)) (cdrs (cdr matrix)))
+  )
+)
+
+; Retirado de https://stackoverflow.com/questions/39943232/matrix-transpose-common-lisp
+(defun transpose (matrix)
+  "Transpose matrix"
+  (cond ((null matrix) NIL)
+        ((null (car matrix)) NIL)
+        (t (cons (cars matrix) (transpose (cdrs matrix))))
+  )
+)
+
 (defun searchSolution (board)
-  (loop for line in board
-    do (loop for elem in line
-      do (if (not (single (getElemValues elem)))
-        (loop for choice in (getElemValues elem)
-          do (let ((newElem (list (list choice) (getElemId elem) (getElemIdx elem))))
-            (let 
-              (
-                (newBoard (matrixMap (function (lambda (el all)
-                  (if (isEqual el elem)
-                    newElem
-                    el
-                  )))
-                  board)
+  (if (valid stop)
+    board
+    (loop for line in board
+      do (loop for elem in line
+        do (if (not (single (getElemValues elem)))
+          (loop for choice in (getElemValues elem)
+            do (let ((newElem (list (list choice) (getElemId elem) (getElemIdx elem))))
+              (let 
+                (
+                  (newBoard (matrixMap (function (lambda (el all)
+                    (if (isEqual el elem)
+                      newElem
+                      el
+                    )))
+                    board)
+                  )
                 )
+                (return-from searchSolution (searchSolution (possibleChoices newBoard)))
               )
-              (return-from searchSolution newBoard)
             )
           )
         )
@@ -192,4 +276,8 @@
   )
 )
 
-(printMap (formatBoard (searchSolution (possibleChoices (indexBoard (board))))))
+;; (printMap (formatBoard (searchSolution (possibleChoices (indexBoard (board))))))
+;; (write (validNeighbours '(1 2 3 4 4)))
+;; (write (transpose '((1 2 3) (4 5 6))))
+(write (valid '(((1) (2)) ((1) (2)))))
+(write (decrease '(5 3 1)))
